3d23f10aa7ba072acf4d01b36b16a1ede2387276
Camiel Adriaens <145980502+CamielAdriaens@users.noreply.github.com>
2025-01-09 13:26:03 +0100

    kestrel endpoint

--- FileStorage/obj/Debug/net8.0/FileStorage.AssemblyInfo.cs
+++ FileStorage/obj/Debug/net8.0/FileStorage.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfaf183d558311c70c001456969e0898d6a612c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f36dc34c25d1c6fd62dc2a0804efcb4a5f3f888")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
